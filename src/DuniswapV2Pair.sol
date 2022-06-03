428402f34ede3d7989f4d4e9f8b87311e7e79aae
debamit <debamit@outlook.com>
2022-06-03 01:09:44 -0700

    Implement swap.

--- src/DuniswapV2Pair.sol
+++ src/DuniswapV2Pair.sol
@@ -3,6 +3,7 @@ pragma solidity ^0.8.9;
 
 import "solmate/tokens/ERC20.sol";
 import "./libraries/Math.sol";
+import "./libraries/UQ112x112.sol";
 
 interface IERC20 {
     function balanceOf(address) external returns (uint256);
@@ -10,21 +11,37 @@ interface IERC20 {
     function transfer(address to, uint256 amount) external;
 }
 
+error BalanceOverflow();
 error InsufficientLiquidityMinted();
 error InsufficientLiquidityBurned();
+error InsufficientOutputAmount();
+error InsufficientLiquidity();
+error InvalidK();
 error TransferFailed();
 
 contract DuniswapV2Pair is ERC20, Math {
+    using UQ112x112 for uint224;
+
     uint256 constant MINIMUM_LIQUIDITY = 1000;
     address public token0;
     address public token1;
 
     uint112 private reserve0;
     uint112 private reserve1;
+    uint32 private blockTimestampLast;
+
+    uint256 public price0CumulativeLast;
+    uint256 public price1CumulativeLast;
 
     event Burn(address indexed sender, uint256 amount0, uint256 amount1);
     event Mint(address indexed sender, uint256 amount0, uint256 amount1);
     event Sync(uint256 reserve0, uint256 reserve1);
+    event Swap(
+        address indexed sender,
+        uint256 amount0Out,
+        uint256 amount1Out,
+        address indexed to
+    );
 
     constructor(address token0_, address token1_)
         ERC20("ZuniswapV2 Pair", "ZUNIV2", 18)
@@ -34,7 +51,7 @@ contract DuniswapV2Pair is ERC20, Math {
     }
 
     function mint() public {
-        (uint256 _reserve0, uint256 _reserve1, ) = getReserves();
+        (uint112 _reserve0, uint112 _reserve1, ) = getReserves();
         // Check contract token balance
         uint256 balance0 = IERC20(token0).balanceOf(address(this));
         uint256 balance1 = IERC20(token1).balanceOf(address(this));
@@ -57,21 +74,9 @@ contract DuniswapV2Pair is ERC20, Math {
 
         _mint(msg.sender, liquidity);
 
-        _update(balance0, balance1);
+        _update(balance0, balance1, _reserve0, _reserve1);
 
         emit Mint(msg.sender, amount0, amount1);
-    }
-
-    function getReserves()
-        public
-        view
-        returns (
-            uint112,
-            uint112,
-            uint32
-        )
-    {
-        return (reserve0, reserve1, 0);
     }
 
     function burn() public {
@@ -92,14 +97,93 @@ contract DuniswapV2Pair is ERC20, Math {
         balance0 = IERC20(token0).balanceOf(address(this));
         balance1 = IERC20(token1).balanceOf(address(this));
 
-        _update(balance0, balance1);
+        (uint112 _reserve0, uint112 _reserve1, ) = getReserves();
+        _update(balance0, balance1, _reserve0, _reserve1);
 
         emit Burn(msg.sender, amount0, amount1);
     }
 
-    function _update(uint256 balance0, uint256 balance1) private {
+    function swap(
+        uint256 amount0Out,
+        uint256 amount1Out,
+        address to
+    ) public {
+        if (amount0Out == 0 && amount1Out == 0)
+            revert InsufficientOutputAmount();
+
+        (uint112 reserve0_, uint112 reserve1_, ) = getReserves();
+
+        if (amount0Out > reserve0_ || amount1Out > reserve1_)
+            revert InsufficientLiquidity();
+
+        uint256 balance0 = IERC20(token0).balanceOf(address(this)) - amount0Out;
+        uint256 balance1 = IERC20(token1).balanceOf(address(this)) - amount1Out;
+
+        if (balance0 * balance1 < uint256(reserve0_) * uint256(reserve1_))
+            revert InvalidK();
+
+        _update(balance0, balance1, reserve0_, reserve1_);
+
+        if (amount0Out > 0) _safeTransfer(token0, to, amount0Out);
+        if (amount1Out > 0) _safeTransfer(token1, to, amount1Out);
+
+        emit Swap(msg.sender, amount0Out, amount1Out, to);
+    }
+
+    function sync() public {
+        (uint112 reserve0_, uint112 reserve1_, ) = getReserves();
+        _update(
+            IERC20(token0).balanceOf(address(this)),
+            IERC20(token1).balanceOf(address(this)),
+            reserve0_,
+            reserve1_
+        );
+    }
+
+    function getReserves()
+        public
+        view
+        returns (
+            uint112,
+            uint112,
+            uint32
+        )
+    {
+        return (reserve0, reserve1, 0);
+    }
+
+    //
+    //
+    //
+    //  PRIVATE
+    //
+    //
+    //
+    function _update(
+        uint256 balance0,
+        uint256 balance1,
+        uint112 reserve0_,
+        uint112 reserve1_
+    ) private {
+        if (balance0 > type(uint112).max || balance1 > type(uint112).max)
+            revert BalanceOverflow();
+
+        unchecked {
+            uint32 timeElapsed = uint32(block.timestamp) - blockTimestampLast;
+
+            if (timeElapsed > 0 && reserve0_ > 0 && reserve1_ > 0) {
+                price0CumulativeLast +=
+                    uint256(UQ112x112.encode(reserve1_).uqdiv(reserve0_)) *
+                    timeElapsed;
+                price1CumulativeLast +=
+                    uint256(UQ112x112.encode(reserve0_).uqdiv(reserve1_)) *
+                    timeElapsed;
+            }
+        }
+
         reserve0 = uint112(balance0);
         reserve1 = uint112(balance1);
+        blockTimestampLast = uint32(block.timestamp);
 
         emit Sync(reserve0, reserve1);
     }
